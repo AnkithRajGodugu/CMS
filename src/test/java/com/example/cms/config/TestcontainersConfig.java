488aab6cca0e49e4e013d41500f2dec02599453e
Uday Kumar Kori <coriuday.18@gmail.com>
2025-09-07 16:54:11 +0530

    pipeline update test-0

--- src/test/java/com/example/cms/config/TestcontainersConfig.java
+++ src/test/java/com/example/cms/config/TestcontainersConfig.java
@@ -1,7 +1,7 @@
 package com.example.cms.config;
 
-import org.springframework.boot.test.context.DynamicPropertyRegistry;
-import org.springframework.boot.test.context.DynamicPropertySource;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 
 public abstract class TestcontainersConfig {
