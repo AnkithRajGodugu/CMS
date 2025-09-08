dfe5dd572b7e6355b006e85f506ebf8684517464
Uday Kumar Kori <coriuday.18@gmail.com>
2025-09-08 13:51:38 +0530

    vite config update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,18 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: import.meta.env.VITE_API_URL, // 🔥 dynamic based on .env
-        changeOrigin: true,
-        secure: false,
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+          secure: false,
+        },
       },
     },
-  },
-});
+  });
+};
